24f750906fb7477a3af22096ef379d948daa7dcc
Mark Strempel <mark.strempel@codecool.com>
2023-11-17 16:03:53 +0100

    ServerApplication logs allowed origins.

--- server/src/main/java/com/codecool/happyshipping/ServerApplication.java
+++ server/src/main/java/com/codecool/happyshipping/ServerApplication.java
@@ -1,5 +1,7 @@
 package com.codecool.happyshipping;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class ServerApplication {
 
+	private final Log logger = LogFactory.getLog(ServerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
@@ -19,6 +23,7 @@ public class ServerApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
+				logger.info("Allow requests from " + allowedOriginPatterns);
 				registry.addMapping("/**").allowedOriginPatterns(allowedOriginPatterns);
 			}
 		};
