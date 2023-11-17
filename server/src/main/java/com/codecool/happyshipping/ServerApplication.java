cba9953a9a9e2f67bc28a77dcc362a586ee898ef
Mark Strempel <mark.strempel@codecool.com>
2023-11-17 15:45:11 +0100

    Add cors config.

--- server/src/main/java/com/codecool/happyshipping/ServerApplication.java
+++ server/src/main/java/com/codecool/happyshipping/ServerApplication.java
@@ -1,13 +1,27 @@
 package com.codecool.happyshipping;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigure(@Value("${cors.allowed-origin-patterns}") String allowedOriginPatterns) {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOriginPatterns(allowedOriginPatterns);
+			}
+		};
 	}
 
 }
