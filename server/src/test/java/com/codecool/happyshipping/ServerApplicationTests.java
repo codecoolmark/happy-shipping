4a6ee8a538975348739a4c96ffc8fd7474e73188
Mark Strempel <mark.strempel@codecool.com>
2023-11-17 16:00:26 +0100

    Fix tests.

--- server/src/test/java/com/codecool/happyshipping/ServerApplicationTests.java
+++ server/src/test/java/com/codecool/happyshipping/ServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.codecool.happyshipping;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = { "cors.allowed-origin-patterns=http://localhost:5173" })
 class ServerApplicationTests {
 
 	@Test
